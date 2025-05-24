b834175035f9735725963a1929e48f71916f6666
Doanator <kdoan0625@gmail.com>
2025-05-23 22:40:13 -0500

    Tournament data building + styling changes
    Font change; footer, tournament table, season filtering, base dynamic tournament details page, base tournament data input page added

--- init_db.py
+++ init_db.py
@@ -13,9 +13,25 @@ cur.execute("""
   );
 """)
 cur.execute("""
+    CREATE TABLE IF NOT EXISTS tournaments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    event TEXT NOT NULL,
+    airdate TEXT,
+    prelim_dates TEXT,
+    city TEXT,
+    oil TEXT,
+    winner TEXT,
+    prize_money TEXT,
+    season TEXT
+  );
+""")
+cur.execute("""
     INSERT INTO players (name, nationality, active_years, titles, image_url) VALUES
     ('Norm Duke', 'America', '1982-2022', 40, 'http://example.com/player1.jpg');
 """)
-print("Inserted player successfully.")
+cur.execute("""
+    INSERT INTO tournaments (event, airdate, prelim_dates, city, oil, winner, prize_money, season) VALUES
+    ('PBA Delaware Classic', 'January 25, 2025', 'January 20-24, 2025', 'Middletown, DE', 'Cheetah', 'Graham Fach (2)', '$30,000', '2025');
+""")
 con.commit()
 con.close()
