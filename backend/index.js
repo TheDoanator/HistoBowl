5e563e6cb5a83fcf8f1e03322eac494f3e4c7139
TheDoanator <kdoan0625@gmail.com>
2026-06-02 12:05:14 -0500

    SQLite connection established with Express server

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,21 @@
 const express = require('express'); // Imports Express
+const sqlite3 = require('sqlite3').verbose(); // Imports SQLite3 in verbose mode for logging purposes
 
 const app = express(); // Creates web server app
+const db = new sqlite3.Database('./histobowl.db'); // Initializes database
 
 app.listen(8000, () => {
   console.log("HistoBowl server is running on port 8000.") // Starts server on port 8000; logs if successful
 });
+
+app.get('/api/tournaments', (req, res) => {
+  const sql = 'SELECT * FROM tournaments'; // Build command to execute
+
+  db.all(sql, [], (err, rows) => { // Executes command on DB
+    if (err) {
+      res.status(500).json({ error: err.message }); // Return error message if encountered
+      return;
+    }
+    res.json(rows); // Converts data in JSON
+  })
+})
